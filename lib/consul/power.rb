b5db8346a920c64374878c9643c2a7e5d6681d31
Henning Koch <henning.koch@makandra.de>
2013-01-16 09:11:22 +0100

    better meta-checks

--- lib/consul/power.rb
+++ lib/consul/power.rb
@@ -35,19 +35,44 @@ module Consul
       include?(*args) or raise Consul::Powerless.new("No power to #{args.inspect}")
     end
 
-    def for_record(*args)
+    def name_for_record(*args)
       adjective, record = Util.adjective_and_argument(*args)
-      for_model(adjective, record.class)
+      name_for_model(adjective, record.class)
     end
 
-    def for_model(*args)
+    def for_record(*args)
+      send(name_for_record(*args))
+    end
+
+    def include_record?(*args)
+      adjective, record = Util.adjective_and_argument(*args)
+      include?(name_for_record(*args), record)
+    end
+
+    def include_record!(*args)
+      adjective, record = Util.adjective_and_argument(*args)
+      include!(name_for_record(*args), record)
+    end
+
+    def name_for_model(*args)
       adjective, model_class = Util.adjective_and_argument(*args)
       collection_name = model_class.name.underscore.gsub('/', '_').pluralize
       [adjective, collection_name].select(&:present?).join('_')
     end
 
-    private
+    def for_model(*args)
+      send(name_for_model(*args))
+    end
 
+    def include_model?(*args)
+      include?(name_for_model(*args))
+    end
+
+    def include_model!(*args)
+      include!(name_for_model(*args))
+    end
+
+    private
 
     def boolean_or_nil?(value)
       [TrueClass, FalseClass, NilClass].include?(value.class)
@@ -87,12 +112,44 @@ module Consul
         end
       end
 
+      def include_model?(*args)
+        if current
+          current.include_model?(*args)
+        else
+          true
+        end
+      end
+
+      def include_model!(*args)
+        if current
+          current.include_model!(*args)
+        else
+          true
+        end
+      end
+
       def for_record(*args)
         if current
           current.for_record(*args)
         else
           adjective, record = Util.adjective_and_argument(*args)
           record.class
+        end
+      end
+
+      def include_record?(*args)
+        if current
+          current.include_record?(*args)
+        else
+          true
+        end
+      end
+
+      def include_record!(*args)
+        if current
+          current.include_record!(*args)
+        else
+          true
         end
       end
 
